5cbe589c3d932d07c908996b788973a846647e33
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2024-09-04 18:19:02 -0400

    [add] more tests

--- tests/functools/test_propagate_error.py
+++ tests/functools/test_propagate_error.py
@@ -1,6 +1,10 @@
 from yootils.functools.propagate_error import Success, propagate_error
 
 
+class ThisShouldNotHappen(Exception):
+    pass
+
+
 def test_success() -> None:
     @propagate_error[Exception]()
     def func(x: int) -> int:
@@ -13,3 +17,33 @@ def test_success() -> None:
         # though in this trivial example we'll never reach this point.
         case uncaught_exception:
             raise uncaught_exception
+
+
+def test_failure_caught_exception() -> None:
+    @propagate_error[ZeroDivisionError]()
+    def func(x: float) -> float:
+        return 1 / x
+
+    match func(0):
+        case Success(_):
+            raise ThisShouldNotHappen()
+        case ZeroDivisionError():
+            pass
+        case uncaught_exception:
+            raise uncaught_exception
+
+
+def test_failure_uncaught_exception() -> None:
+    @propagate_error[ZeroDivisionError]()
+    def func(x: float) -> float:
+        raise ValueError("Uncaught exception")
+        return 1 / x
+
+    match func(0):
+        case Success(_):
+            raise ThisShouldNotHappen()
+        case ZeroDivisionError():
+            raise ThisShouldNotHappen()
+        case uncaught_exception:
+            assert isinstance(uncaught_exception, ValueError)
+            assert str(uncaught_exception) == "Uncaught exception"

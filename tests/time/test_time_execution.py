4daad50898fd300805d2c10a92b9c97c1a4494e0
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2024-09-05 23:17:25 -0400

    [add] batch async test

--- tests/time/test_time_execution.py
+++ tests/time/test_time_execution.py
@@ -5,6 +5,8 @@ import pytest
 
 from yootils.time.time_execution import time_execution, time_execution_async
 
+TOLERANCE = 0.01
+
 
 def test_time_execution() -> None:
     SECONDS_TO_SLEEP = 1
@@ -12,21 +14,21 @@ def test_time_execution() -> None:
     with time_execution() as timer:
         time.sleep(SECONDS_TO_SLEEP)
 
-    assert timer.seconds_elapsed == pytest.approx(SECONDS_TO_SLEEP, rel=0.01)
+    assert timer.seconds_elapsed == pytest.approx(SECONDS_TO_SLEEP, rel=TOLERANCE)
 
 
 def test_time_execution_reuse() -> None:
     with time_execution() as timer:
         time.sleep(1)
 
-    assert timer.seconds_elapsed == pytest.approx(1, rel=0.01)
+    assert timer.seconds_elapsed == pytest.approx(1, rel=TOLERANCE)
 
     # This is discouraged on a typing level but not prevented during runtime
     # If possible, please avoid reusing the timer instance
     with timer() as timer:  # type: ignore[misc]
         time.sleep(2)
 
-    assert timer.seconds_elapsed == pytest.approx(2, rel=0.01)
+    assert timer.seconds_elapsed == pytest.approx(2, rel=TOLERANCE)
 
 
 @pytest.mark.asyncio
@@ -36,7 +38,7 @@ async def test_time_execution_async() -> None:
     async with time_execution_async() as timer:
         await asyncio.sleep(SECONDS_TO_SLEEP)
 
-    assert timer.seconds_elapsed == pytest.approx(SECONDS_TO_SLEEP, rel=0.01)
+    assert timer.seconds_elapsed == pytest.approx(SECONDS_TO_SLEEP, rel=TOLERANCE)
 
 
 @pytest.mark.asyncio
@@ -44,11 +46,28 @@ async def test_time_execution_async_reuse() -> None:
     async with time_execution_async() as timer:
         await asyncio.sleep(1)
 
-    assert timer.seconds_elapsed == pytest.approx(1, rel=0.01)
+    assert timer.seconds_elapsed == pytest.approx(1, rel=TOLERANCE)
 
     # This is discouraged on a typing level but not prevented during runtime
     # If possible, please avoid reusing the timer instance
     async with timer() as timer:  # type: ignore[misc]
         await asyncio.sleep(2)
 
-    assert timer.seconds_elapsed == pytest.approx(2, rel=0.01)
+    assert timer.seconds_elapsed == pytest.approx(2, rel=TOLERANCE)
+
+
+@pytest.mark.asyncio
+async def test_time_execution_async_multiple() -> None:
+    async def sleep(seconds: float) -> float:
+        async with time_execution_async() as timer:
+            await asyncio.sleep(seconds)
+
+        return timer.seconds_elapsed
+
+    batch_seconds_elapsed = await asyncio.gather(
+        sleep(1),
+        sleep(2),
+        sleep(3),
+    )
+
+    assert batch_seconds_elapsed == pytest.approx([1, 2, 3], rel=TOLERANCE)

fa7e1cc71a35e57db9436658f33c22aa70d6e7be
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2024-09-05 23:12:11 -0400

    [add] missing packages

--- tests/google/auth/test_get_service_account_oidc_jwt.py
+++ tests/google/auth/test_get_service_account_oidc_jwt.py
@@ -1,5 +1,10 @@
 import pytest
 
+from yootils.google.auth.get_service_account_oidc_jwt import (  # noqa: F401
+    get_service_account_oidc_jwt,
+    get_service_account_oidc_jwt_async,
+)
+
 
 @pytest.mark.skip(reason="Not implemented yet")
 def test_get_service_account_oidc_jwt() -> None:
